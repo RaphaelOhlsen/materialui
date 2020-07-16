2349f73cc2dd7ade36d58f440dfdfeba1f8b646b
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-07-16 10:40:07 -0300

    56. Social Media Icons

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,7 +4,11 @@ import { makeStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 
-import footerAdornment from '../../assets/Footer Adornment.svg'
+import footerAdornment from '../../assets/Footer Adornment.svg';
+import facebook from '../../assets/facebook.svg';
+import twitter from '../../assets/twitter.svg';
+import instagram from '../../assets/instagram.svg';
+
 
 const useStyles = makeStyles(theme => ({
   footer: {
@@ -23,7 +27,7 @@ const useStyles = makeStyles(theme => ({
       width: "15em"
     }
   },
-  mainContainer :{
+  mainContainer: {
     position: "absolute",
   },
   link: {
@@ -35,6 +39,22 @@ const useStyles = makeStyles(theme => ({
   },
   gridItem: {
     padding: "3em"
+  },
+  icon: {
+    height: "4em",
+    width: "4em",
+    [theme.breakpoints.down("xs")]: {
+      height: "2.5em",
+      width: "2.5em"
+    }
+  },
+  socialContainer: {
+    position: "absolute",
+    marginTop: "-6em",
+    right: "1.5em",
+    [theme.breakpoints.down("xs")]: {
+      right: "0.6em"
+    }
   }
 }));
 
@@ -110,6 +130,37 @@ export default function Footer(props) {
         alt="black decorative slash"
         className={classes.adornment}  
       />
+      <Grid 
+        container 
+        justify="flex-end" 
+        className={classes.socialContainer}
+        spacing={2}
+      >
+        <Grid 
+          item component={"a"}
+          href="http://www.facebook.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={facebook} alt="facebook logo" className={classes.icon}/>
+        </Grid>
+        <Grid 
+          item component={"a"}
+          href="http://www.twitter.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={twitter} alt="twitter logo" className={classes.icon}/>
+        </Grid>
+        <Grid 
+          item component={"a"}
+          href="http://www.instagram.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={instagram} alt="instagram logo" className={classes.icon}/>
+        </Grid>
+      </Grid>
     </footer>      
   )
 }
